687101c168812ac097c94c71d40a92c7c73ecc23
Hadi14 <asusn43@yahoo.com>
2022-12-31 10:28:19 +0330

    ok

---
+++ src/Header/header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header2 from './header2'
+
+export default function header() {
+    return (
+        <div>
+            <Header2 name='Reaza'></Header2>
+            <Header2 name='Ahmad'></Header2>
+            <Header2 name='Ali'></Header2>
+        </div>
+
+    )
+}
---
+++ src/Header/header2.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function header2(pr) {
+    return (
+        <div>{pr.name}</div>
+    )
+}
