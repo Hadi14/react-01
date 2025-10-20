d70d295a43f899182afad9b6cb59a42f7ae42c77
Hadi14 <asusn43@yahoo.com>
2025-10-20 06:40:18 +0330

    add media

--- src/Footer.js
+++ src/Footer.js
@@ -1,6 +1,10 @@
+import Media from "./Media";
 function Footer() {
     return(
-        <div>This is Footer</div>
+        <div>This is Footer
+
+            <Media></Media>
+        </div>
     );
 }
 export default Footer;
---
+++ src/Media.js
@@ -0,0 +1,12 @@
+function Media() {
+    return(
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+        </ul>
+    );
+}
+
+export default Media;
